975cf6bb531dc81b8844a994f159a69bd2672f8d
RainbowTyger <megs.c.dt@gmail.com>
2020-04-21 11:10:32 +0200

    CHANGES = 0

---
+++ Testing.R
@@ -0,0 +1,6 @@
+File > New > R Scrip
+File > New File > R Script
+print("This file was created within RStudio")
+print("And now it lives on GitHub")
+savehistory("C:/Users/User/Desktop/R/New/New.Rproj")
+savehistory("C:/Users/User/Desktop/R/New/Testing.R")
